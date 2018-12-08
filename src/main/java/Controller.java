4cd78ec3420bc605335d5dd0375c1d906f6a21c9
Arek <arek.debski2@gmail.com>
2018-12-08 16:23:29 +0100

    Wstępny zarys gui

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,46 @@
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class Controller {
 
-    public BorderPane rootBorderPane(){
+    public Parent root(){
         BorderPane borderPane = new BorderPane();
+        borderPane.setTop(menu());
+        borderPane.setRight(grid());
+        borderPane.setCenter(pane());
+        borderPane.setBottom(label());
         return borderPane;
+    }
+
+    public Node menu(){
+        MenuBar menuBar = new MenuBar();
+        Menu gameMenu = new Menu("Game");
+        MenuItem newGame = new MenuItem("New Game");
+        return menuBar;
+    };
+
+    public Node grid(){
+        GridPane gridPane = new GridPane();
+
+        return gridPane;
+    }
+
+    public Node pane(){
+        Pane pane = new Pane();
+
+        return pane;
+    }
+
+    public Node label(){
+        Label label = new Label();
+
+        return label;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -7,7 +8,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Checkers");
-        primaryStage.setScene(new Scene(new Controller().rootBorderPane()));
+        primaryStage.setScene(new Scene(new Controller().root()));
         primaryStage.show();
     }
 
